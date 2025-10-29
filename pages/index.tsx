adfea6090a0786df25b18a46c86d0b5537f1d2e1
wmsbussines787 <wmsbussines787@gmail.com>
2025-10-29 06:38:58 +0000

    feat: páginas base y API /api/recipes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{maxWidth: 720, margin: '40px auto', fontFamily:'system-ui'}}>
       <h1>Recetas App</h1>
-      <p>API lista en <code>/api/recipes</code>. Inserta una receta con POST y aparecerá en la BD.</p>
+      <p>API lista en <code>/api/recipes</code>.</p>
     </main>
   );
 }
